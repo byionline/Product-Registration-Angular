039e45248768b57be90dee4ce77b5a393caa305d
byionline <10815566+byionline@users.noreply.github.com>
2020-08-05 15:44:05 +0530

    Fetch Data from Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductService } from './service/product.service';
+import { AdminComponent } from './components/admin/admin.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, AdminComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+})
+export class AdminComponent implements OnInit {
+  public products;
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts() {
+    this.productService.getProducts().subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (err) => console.error(err),
+      () => console.log('products loaded')
+    );
+  }
+}
